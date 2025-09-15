0d6eb58f3ec8aeb45d3d309271d1df565c982926
afranco <173817429+Alex-af-2024@users.noreply.github.com>
2025-09-14 22:41:52 -0300

    subida 14-09 22:41

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit
+
+# instalar deps (por si Render no lo hace)
+pip install -r requirements.txt
+
+# recopila archivos estáticos (necesario si usas WhiteNoise)
+python manage.py collectstatic --no-input
+
+# aplica migraciones
+python manage.py migrate
+
---
+++ cliente/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class Cliente(models.Model):
+    nombre  = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    edad = models.IntegerField()
+    profesion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
